f08f265fce62bb6d7424b23b6b07a3c697c411d7
mathitejeda <mathi.tejeda@gmail.com>
2023-06-02 20:24:33 -0300

    Agregue una clase para el carrito, es bastante simple pero si se quiere trabajar sobre base de datos o con usuarios es conveniente tener una ya que esta puede contener datos necesarios para el usuario y demas

---
+++ Modelo/Carrito.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Carrito
+    {
+        public Articulo articulo { get; set; }
+        public int cantidad { get; set; }
+        public Carrito(Articulo articulo, int cantidad)
+        {
+            this.articulo = articulo;
+            this.cantidad = cantidad;
+        }
+    }
+}
